c2f5430ead99f1e83eff8b07a0ec2853630a486f
liyi <1341821655@qq.com>
2020-03-10 13:24:21 +0800

    删除多余路由

--- vue_ec/src/router/actionRouter.js
+++ vue_ec/src/router/actionRouter.js
@@ -5,9 +5,6 @@ import UserMsg from '@/views/user_msg'
 import Home from '@/views/home'
 import Goods from '@/views/goods'
 import Order from '@/views/order'
-import TabIndex from '@/views/tabIndex'
-import FullServiceList from '@/views/fullServiceList'
-import ComplaintsClass from '@/views/complaintsClass'
 
 
 
@@ -17,13 +14,7 @@ export default[
             {path: '/Home',name: 'home',component: Home},
             {path: '/UserMsg',name: 'userMsg',component: UserMsg},
             {path: '/Goods',name: 'goods',component: Goods},
-            {path: '/Order',name: 'order',component: Order},
-            {path: '/TabIndex',name: 'tabIndex',component: TabIndex,
-                children:[
-                    {path: '/ComplaintsClass',name: 'complaintsClass',component: ComplaintsClass},
-                    {path: '/FullServiceList',name: 'fullServiceList',component: FullServiceList}
-                ]
-            },
+            {path: '/Order',name: 'order',component: Order}
         ]
     },   
 ]
